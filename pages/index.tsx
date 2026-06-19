6b9afd8415d312b9a0b15ed6fa0aeca8f7e732ba
chika-ssy <sen.chika12321@gmail.com>
2026-06-19 15:13:32 +0900

    シェアボタンの文言修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -232,7 +232,7 @@ export default function Home() {
                 onClick={handleShareX}
                 className="w-full bg-sky-600 hover:bg-sky-800 text-white font-black py-3.5 px-6 rounded-xl border-4 border-slate-700 shadow-[3px_3px_0px_0px_rgba(51,65,85,1)] transition-all duration-100 hover:translate-x-[1px] hover:translate-y-[1px] hover:shadow-[2px_2px_0px_0px_rgba(51,65,85,1)] active:translate-x-[3px] active:translate-y-[3px] active:shadow-[0px_0px_0px_0px_rgba(51,65,85,1)] text-center text-sm flex items-center justify-center gap-2"
               >
-                <span>𝕏</span> 結果をエックスでポストする
+                <span>𝕏</span> 結果をシェアする
               </button>
             </div>
 
